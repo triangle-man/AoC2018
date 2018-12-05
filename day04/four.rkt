da965f2ff854002cef92affd5455097528352f64
James Geddes <jgeddes@turing.ac.uk>
2018-12-05 12:08:40 +0000

    Working on day four

--- day04/four.rkt
+++ day04/four.rkt
@@ -7,18 +7,23 @@
 (define MONTH-LENGTH '(31 28 31 30 31 30 31 31 30 31 30 31))
 
 ;; Add one day to a date
-(define (date-bump dt)
+(define (date-add1 dt)
   (match-define (Date year month day) dt)
-  (define month-length (list-ref MONTH-LENGTH month))
-  (Date year
-        month
-        (modulo (+ day 1) (list-ref MONTH-LENGTH)))
+  (define month-length (list-ref MONTH-LENGTH (- month 1)))
+  (define-values (month-inc new-day)
+    (quotient/remainder day month-length))
+  (define-values (year-inc new-month)
+    (quotient/remainder (+ (- month 1) month-inc) 12))
+  (define new-year (+ year year-inc))
+  (Date new-year
+        (+ new-month 1)
+        (+ new-day 1)))
 
-  ;; An Oclock is an hour and a minute
-  (struct Oclock (hour minute) #:transparent))
+;; An Oclock is an hour and a minute
+(struct Oclock (hour minute) #:transparent)
 
 ;; An Event is either a guard coming on duty, a falling-asleep or a waking-up
-(struct Event (Date Oclock) #:transparent)
+(struct Event (dt oc) #:transparent)
 (struct Event/duty Event (guard) #:transparent)
 (struct Event/asleep Event () #:transparent)
 (struct Event/awaken Event () #:transparent)
@@ -42,15 +47,12 @@
 (define (parse-event s)
   (match-define (list yr mn dy hr mi descr guard)
     (cdr (regexp-match EVENT-RE s)))
-  (let ([year   (string->number yr)]
-        [month  (string->number mn)]
-        [day    (string->number dy)]
-        [hour   (string->number hr)]
-        [minute (string->number mi)])
+  (let ([dt (Date (string->number yr) (string->number mn) (string->number dy))]
+        [tm (Oclock (string->number hr) (string->number mi))])
     (cond
-      [(string=? descr "wakes up") (Event/awaken year month day hour minute)]
-      [(string=? descr "falls asleep") (Event/asleep year month day hour minute)]
-      [else (Event/duty year month day hour minute (string->number guard))])))
+      [(string=? descr "wakes up") (Event/awaken dt tm)]
+      [(string=? descr "falls asleep") (Event/asleep dt tm)]
+      [else (Event/duty dt tm (string->number guard))])))
 
 (define events (map parse-event inputs))
 
@@ -62,8 +64,21 @@
 ;; four.rkt> (remove-duplicates (map Event-hour events))
 ;; '(0 23)
 
-;; Step 1. Figure out 
+;; Step 1. Figure out which guard is on duty on a particular day. When a guard
+;; comes on duty in the 23hr hour, their duty date is the next day.
 
+(define (maybe-inc-date dt oc)
+  (if (= (Oclock-hour oc) 23)
+      (date-add1 dt)
+      dt))
+
+;; roster : List-of (Date . number?), where number is the guard's id
+(define roster
+  (map (λ (ev) (cons (maybe-inc-date (Event-dt ev) (Event-oc ev)) (Event/duty-guard ev)))
+       (filter Event/duty? events)))
+
+;; Step 2. For each day, put the events in order and compute the times at which
+;; guards were asleep or awake
 
 ;;; Part 2
 ;;; --------------------------------------------------------------------------------
